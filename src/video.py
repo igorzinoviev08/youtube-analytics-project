3f8c88e7ba2095bbb0a2c3d2f6a55a73e4195df4
igorzinoviev08 <zinovievigor08@gmail.com>
2023-08-04 02:05:48 +0300

    api_key HW_6 is exec

--- src/video.py
+++ src/video.py
@@ -4,12 +4,22 @@ from src.channel import Channel
 class Video:
     def __init__(self, video_id: str) -> None:
         self.video_id = video_id
-        youtube = Channel.get_service().videos().list(part='snippet,statistics', id=self.video_id).execute()
-        video_data = youtube.get('items')[0]
-        self.title = video_data.get('snippet').get('title')
-        self.url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count = video_data.get('statistics').get('viewCount')
-        self.like_count = video_data.get('statistics').get('likeCount')
+        try:
+
+            youtube = Channel.get_service().videos().list(part='snippet,statistics', id=self.video_id).execute()
+            video_data = youtube.get('items')[0]
+        except IndexError:
+            print('невозможно получить данные')
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+
+        else:
+            self.title = video_data.get('snippet').get('title')
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = video_data.get('statistics').get('viewCount')
+            self.like_count = video_data.get('statistics').get('likeCount')
 
     def __str__(self) -> str:
         return self.title
